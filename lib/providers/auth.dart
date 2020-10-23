57453005ffb79bd90c341601b0eace2fef2300d5
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-10-23 23:28:24 +0300

    Login Added

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,13 +3,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Auth with ChangeNotifier {
+  static String _apiKey = "AIzaSyCIfeMSm9wgbY-cwEL4WTs7MvWse7q70jA";
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    final url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCIfeMSm9wgbY-cwEL4WTs7MvWse7q70jA";
+  Future<void> _authenticate(
+      String email, String password, String endpoint) async {
+    final url = endpoint;
     final response = await http.post(
       url,
       body: json.encode({
@@ -18,5 +19,19 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true,
       }),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
+            _apiKey;
+    return _authenticate(email, password, url);
+  }
+
+  Future<void> login(String email, String password) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+            _apiKey;
+    return _authenticate(email, password, url);
   }
 }
