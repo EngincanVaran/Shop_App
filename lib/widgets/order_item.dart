b294b0be9145531835e4b0a186bb5bafa5285e1f
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-10-27 11:11:42 +0300

    Expanding Lists Animation, sooo smooth

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,30 +18,38 @@ class _OrderListItemState extends State<OrderListItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text("\$${widget.order.amount}"),
-            subtitle: Text(
-              DateFormat("dd/MM/yyyy hh:mm").format(
-                widget.order.dateTime,
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded
+          ? min((widget.order.products.length * 20.0 + 110), 200)
+          : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("\$${widget.order.amount}"),
+              subtitle: Text(
+                DateFormat("dd/MM/yyyy hh:mm").format(
+                  widget.order.dateTime,
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
+            // if (_expanded)
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-              height: min((widget.order.products.length * 20.0 + 20), 100),
+              height: _expanded
+                  ? min((widget.order.products.length * 20.0 + 20), 100)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map((product) => Row(
@@ -64,7 +72,8 @@ class _OrderListItemState extends State<OrderListItem> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
