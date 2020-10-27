b78c74dc80070fd043930b0f0d9d32570ee3c2bc
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-10-27 10:59:46 +0300

    Hero Animation, image slide to screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -25,9 +25,12 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+              child: Hero(
+                tag: productID,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
